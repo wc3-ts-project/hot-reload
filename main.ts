309fb68013f34a69a35c8de89eb65e8f84d9f884
WriteCoin <sovtsardom@gmail.com>
2022-02-11 22:23:00 +0300

    подготовлен функционал hot-reload

--- main.ts
+++ main.ts
@@ -2,8 +2,13 @@ import "./modules/utils/index"
 import "./modules/obj-edit/index"
 import "./modules/obj-edit/test"
 import { initGlobals } from "./src/global"
+import { setTimeout } from "./modules/utils/timer"
+import "./modules/wts-parser/index"
+import "./modules/hot-reload/index"
 
 InitGlobals = () => {
     initGlobals()
-    require("src.index")
+    setTimeout(0., () => {
+        require("src.index")
+    })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-print('Hello TypeScriptToLua!')
+import { Footman } from "../modules/obj-edit/test"
+import { initArray } from "../modules/utils/array"
+import { printTable } from "../modules/utils/object"
+import { fourCC } from "../modules/utils/other"
+
+// const u = CreateUnit(Player(0), FourCC("hpea"), 0, 0, 0)
+// print(IsUnitInForce(u, bj_FORCE_ALL_PLAYERS))
+// print(IsUnitInForce(u, bj_FORCE_PLAYER[0]))
+// print(IsUnitInForce(u, bj_FORCE_PLAYER[1]))
+// print(IsUnitInForce(u, bj_FORCE_PLAYER[10]))
+
+// const force = CreateForce()
+// ForceAddPlayer(force, Player(0))
+
+// print(IsUnitInForce(u, force))
+
+const arr = initArray([1, 2, 3], 3)
+printTable(arr, 'arr')
+
+
+const footman = CreateUnit(Player(0), fourCC(Footman.id), 0, 0, 0)
+print(GetUnitName(footman))
